cfcccc66d0cf71d31fcd13bc6a28e4e8475bdd16
Sandesh Sarade <Saradesandesh@gmail.com>
2024-03-05 16:39:26 +0530

    added arraylist with sout comment list od data integer code for city

--- GitDemo/src/com/velocity/ArrayListDemo2.java
+++ GitDemo/src/com/velocity/ArrayListDemo2.java
@@ -1,9 +1,18 @@
 package com.velocity;
 
+import java.util.ArrayList;
+
 public class ArrayListDemo2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(450);
+		list.add(150);
+		list.add(650);
+		list.add(850);
+		list.add(250);
+		System.out.println(list);
+		
 
 	}
 
