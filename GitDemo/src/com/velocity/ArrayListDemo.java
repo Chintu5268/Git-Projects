8dc7960fb5336a878ef90526dbef8fe319a93842
Sandesh Sarade <Saradesandesh@gmail.com>
2024-03-05 14:40:00 +0530

    added arraylist code for city

---
+++ GitDemo/src/com/velocity/ArrayListDemo.java
@@ -0,0 +1,19 @@
+ package com.velocity;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList();
+		al.add("sandesh");
+		al.add("Rahul");
+		al.add("Shivam");
+		al.add("Pranay");
+		al.add("Akash");
+		al.add("Piyush");
+		System.out.println(al);
+
+
+	}
+
+}
